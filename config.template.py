dd3ce7fca599d774a988ff91ec1c6048a599ae59
Joseph Moussa <joeph465@gmail.com>
2021-03-02 01:00:46 -0500

    add 3 security keys in config template for future use

--- config.template.py
+++ config.template.py
@@ -3,5 +3,8 @@ MONGODB_DB_NAME = "anonymizer"
 MAX_CONNECTIONS_COUNT = 10
 MIN_CONNECTIONS_COUNT = 1
 JWT_TOKEN_PREFIX = "Token"
-SECRET_KEY = ""
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7  # one week
+# These secret keys will ideally be coming from different sources
+SECRET_KEY_1 = ""
+SECRET_KEY_2 = ""
+SECRET_KEY_3 = ""
